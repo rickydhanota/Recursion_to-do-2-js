933119b37a0f8a968c6407484b85d9144a289dad
Ricky <rickdhanota@gmail.com>
2020-08-19 20:22:01 -0700

    added a countdown function

--- to-do-2.js
+++ to-do-2.js
@@ -1,4 +1,4 @@
-//Write rFib(num). Recursively compute and return numth Fibonacci value. As earlier, treat first two (num = 0, num = 1) Fibonacci vals as 0 and 1. 
+//1. Write rFib(num). Recursively compute and return numth Fibonacci value. As earlier, treat first two (num = 0, num = 1) Fibonacci vals as 0 and 1. 
 
 //rFib(2) = 1 (0+1); 
 
@@ -22,3 +22,18 @@ function rFib(num){//Initial function call is rFib(5)
 }
 console.log(rFib(9));//1+1+1+0+1+0+1
 //Fibonacci = 0(position 0), 1(position 1), 1, 2, 3, 5
+
+
+//2. Write function rTrib(num) to mimic Fibonacci, adding previous three values instead of two. First three Tribonacci numbers are 0, 0, 1, so rTrib(3) = 1 (0+0+1); rTrib(4) = 2 (0+1+1); rTrib(5) = 4 (1+1+2); rTrib(6) = 7 (1+2+4). Handle negatives and non-integers appropriately and inexpensively.
+
+function countdown(num){
+    if(num == 0){//the only base case here in this countdown function is 0, and the reason why is because  
+        console.log(0);
+        return
+    }
+    else{
+        console.log(num);
+        countdown(num-1);
+    }
+}
+console.log(countdown(10));
